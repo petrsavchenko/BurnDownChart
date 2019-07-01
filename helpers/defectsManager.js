04dbbb48084cad075b1122bb3776ca71730a2292
PetrSavchenko <petr.savchenko@plutora.com>
2019-07-01 19:44:27 +1000

    google deployment file has been added and plutora test urls updated

--- helpers/defectsManager.js
+++ helpers/defectsManager.js
@@ -4,6 +4,10 @@ class DefectsManager {
         return Math.round(value * multiplier) / multiplier;
     }
 
+    base() {
+        return;
+    }
+
     getDiffDays(startDate, endDate) {
         const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
         const diffDays = Math.round(Math.abs((startDate.getTime() - endDate.getTime()) / (oneDay)));
