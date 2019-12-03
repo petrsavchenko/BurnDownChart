1ebf5b065e1b0382b47224488c245a9e237dd8b5
Petr Savchenko <petr.savchenko@plutora.com>
2019-12-03 16:05:45 +1100

    access token updated (grabbed from decoded ApplicationData cookie)

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
         getAuthUrl: 'https://home.plutoratest.org/api/authentication/auth/refresh',
         refresh_token: 'hAtQb7vG4yaTiyXgDX/+G9PuVs4ov1AZ4GfNXP06FEa8s1IviHNkU/WB7rxNyQdK0VcG8abg2c4FRQmwePGHVw==',
         // due to secutity changes in plutora test
-        access_token: '1E8zezg/x0LfmhOMEwBOBNV6DTJElBYCpIDHjL0cNVHmLBw6ExAqLfEt8IA1t3cPd0eiDExsXezHC8LWlvd+z/8yod6J8Wx2USTHsy7Wa7fEA7htKTCmibbtkBKBZY7UXFFIn5lBgL5snFKCAFqhnJEnovZDnu2A27m31FidmJmohxkrNE514sElz0X51ktlz+W1NzBBvV/nlHPffRHxrAAYOoLGzIvr3nDbaspmbUd1fm/uIPvvPogeEpFTqZ1VHrradw9zvsJTShXPyCrjo33sa3mlehxmINblV4zJvtSW9finI6rJf2nRHVUW7K5KgLJlNBKtFqSC/V4WYAJl9wlwlMk5yfDW/ZS5tZbkDTQ3dKebE05llpJ0rL6p/zZjAS2YEn5WexaDwYxjbe1TrSEo4WMcvzS+IiXLbuX426wqtdGyLW9vG2NOK4EV5iSzeHxxPVuyYIMW6iiluH4+WK7P7gYZJBVlS+7PowT83RcrQGt1vRSRMnz3xdyU2I5/HTbbpbYEfzn689brByUMixGjGmkDFcgMAAazFb16xhPciHkYyG0WbsQc70BDLFzh5BVND5C6VGUkFxlTwuTEbMpfo5hFRSJNpZGcDzyZqBwSUAvQ'
+        access_token: 'L/TzYxENX8c5KO3G0b1eO9ciMcF1ki/GRQ360tG/9hyK7CwISlASJa7S+3wY43UTjjekkX5XM/6TlRwS2J+ppIVihLEOage4tFvXGJnKfoURFJQM8CYIsZgddzyoVEa5H/8NN9wyKhkh8jqiR+l/vwOC6FRk9xyhETTdzUfG1WKGuO1hk/XenRclNRVFg24Rc8NHLj7os7JngNxfaHZElKf2ifPpdCFudVAEEf2FMIht03sJKm7+IFZfMG2q8GqcNGl5w+W74X3mAUVAeAyAbMW8hq0qqhulgyMYTMjICuMyuHXy/pPq/bXq5nSIfRg5sVlXmw9nHUGcxFeCASL8Os6g1+OrsveAMrVOrC4hidfTDXX/EDcqsxlma9f62zP4qSSpg3oPS3fLCrmhZxObKn4zYYPm3ltJlFHRVJKfBvWUjYF2wB3PSYVSClLVxzpUbv+xgCg9k0rzD/FH9Mmb9y1GQvrpQL71VkrrPRaI+b5xCIR6TlT6hgsvPM1zr1mTPVFQ6gdtjL/KP0CwDFggL77Ndi3Z0PAc3rteS5BUcN3UwfUvVUIS0//koQT76QK9XGcQVzX43RRhOMXM2eESYV03tgvplLNv/PsSlwIfUQB/e5dE'
     },    
     db: {
         uri: 'mongodb+srv://pricechecker:gtnh5512+@pricechecker-bsqyh.mongodb.net/burnDownChart?retryWrites=true' 
